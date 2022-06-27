0b97594705418c34b285d5e5c4034080fb1146bc
Evangelos Poneres <evangelos@even.health>
2022-06-27 08:50:18 -0400

    Init

---
+++ src/main/java/io/meraklis/happy_tenant/HappyTenantApplication.java
@@ -0,0 +1,13 @@
+package io.meraklis.happy_tenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTenantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyTenantApplication.class, args);
+	}
+
+}
